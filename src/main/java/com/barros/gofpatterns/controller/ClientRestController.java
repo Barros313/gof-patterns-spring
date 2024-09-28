88cbeb3a5ef822342dedefc530d1273823f5e68d
Gabriel Barros de Almeida <gbarros1337@gmail.com>
2024-09-28 11:57:37 -0300

    Saving changes
    Signed-off-by: Gabriel Barros de Almeida <gbarros1337@gmail.com>

---
+++ src/main/java/com/barros/gofpatterns/controller/ClientRestController.java
@@ -0,0 +1,21 @@
+package com.barros.gofpatterns.controller;
+
+import com.barros.gofpatterns.model.Client;
+import com.barros.gofpatterns.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ClientRestController {
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<Iterable<Client>> findAll() {
+        return ResponseEntity.ok(clientService.findAll());
+    }
+}
--- src/main/java/com/barros/gofpatterns/service/ViaCepService.java
+++ src/main/java/com/barros/gofpatterns/service/ViaCepService.java
@@ -1,7 +1,14 @@
 package com.barros.gofpatterns.service;
 
+import com.barros.gofpatterns.model.Address;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@FeignClient(name = "viacep", url="https://viacep.com.br/ws")
 public interface ViaCepService {
-    @RequestMapping(method = )
+    @GetMapping("/{cep}/json/")
+    Address findAddress(@PathVariable("cep") String cep);
 }
