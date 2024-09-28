530291d609ce89e6e8485cec93862443441fb2ea
Barros313 <gbarros1337@gmail.com>
2024-09-28 20:50:14 -0300

    Added javadoc to main file
    Signed-off-by: Barros313 <gbarros1337@gmail.com>

--- src/main/java/com/barros/gofpatterns/GofPatternsApplication.java
+++ src/main/java/com/barros/gofpatterns/GofPatternsApplication.java
@@ -8,6 +8,19 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 public class GofPatternsApplication {
 
+    /**
+     * Projeto Spring Boot gerado via IntelliJ Ultimate
+     * Módulos selecionados:
+     * <ul>
+     *   <li>Spring Data JPA</li>
+     *   <li>Spring Web</li>
+     *   <li>H2 Database</li>
+     *   <li>OpenFeign</li>
+     *   <li>JDBC API</li>
+     * </ul>
+     *
+     * JDBC e Postgres serão utilizados posteriormente
+     */
     public static void main(String[] args) {
         SpringApplication.run(GofPatternsApplication.class, args);
     }
